cf8565554ccaf91e4f62ecb929e47412f2de5424
AndriOkta <andreroger167@gmail.com>
2022-01-07 14:26:51 +0700

    Commit Terakhir

--- src/app/division/division.component.spec.ts
+++ src/app/division/division.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DivisionComponent } from './division.component';
 
+
 describe('DivisionComponent', () => {
   let component: DivisionComponent;
   let fixture: ComponentFixture<DivisionComponent>;
